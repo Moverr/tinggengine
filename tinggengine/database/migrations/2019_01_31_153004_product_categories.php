454c2af71cc4c1691559ea2aa10e22c4592b91f3
Muyinda Rogers <moverr@gmail.com>
2019-02-01 00:20:11 +0300

    :Debugigng :

--- tinggengine/database/migrations/2019_01_31_153004_product_categories.php
+++ tinggengine/database/migrations/2019_01_31_153004_product_categories.php
@@ -4,25 +4,23 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class ProductCategories extends Migration
 {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    private  $tablename = 'users';
-    
+  private  $tablename = 'productcategories';
 
     public function up()
     {
         Schema::create($this->tablename, function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('username')->unique();
-            $table->string('password'); 
-            $table->enum('status',array('ACTIVE','PENDING','ARCHIVED'))->default(DB::raw('PENDING'));
-            $table->bigInteger('profile_id')->nullable();
-            $table->bigInteger('created_by')->nullable();
+            $table->string('name')->unique();
+            $table->string('code')->unique();
+            $table->enum('status',array('ACTIVE','ARCHIVED'));
+            $table->bigInteger('created_by');
             $table->timestamp('date_created')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->bigInteger('updated_by')->nullable();
             $table->datetime('date_updated')->nullable(); 
--- tinggengine/database/migrations/2019_01_31_181944_products.php
+++ tinggengine/database/migrations/2019_01_31_181944_products.php
@@ -4,25 +4,25 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class Products extends Migration
 {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    private  $tablename = 'users';
+    private  $tablename = 'products';
     
 
     public function up()
     {
         Schema::create($this->tablename, function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('username')->unique();
-            $table->string('password'); 
-            $table->enum('status',array('ACTIVE','PENDING','ARCHIVED'))->default(DB::raw('PENDING'));
-            $table->bigInteger('profile_id')->nullable();
-            $table->bigInteger('created_by')->nullable();
+            $table->string('name')->unique();
+            $table->string('code')->unique();
+            $table->bigInteger('category_id');
+            $table->enum('status',array('ACTIVE','ARCHIVED'));
+            $table->bigInteger('created_by');
             $table->timestamp('date_created')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->bigInteger('updated_by')->nullable();
             $table->datetime('date_updated')->nullable(); 
