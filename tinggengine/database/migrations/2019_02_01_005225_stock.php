232ae8f1810bc2cbdd40585de6df306a40eb215c
Muyinda Rogers <moverr@gmail.com>
2019-02-01 03:59:27 +0300

    :Debugging :

---
+++ tinggengine/database/migrations/2019_02_01_005225_stock.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Stock extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+   private  $tablename = 'stock';
+    
+
+/*
+TODO: MEASUREMENT SCENARIOS IS AN IMPORTANT ASPECT OF THIS APPLICATION 
+ */
+
+    public function up()
+    {
+        Schema::create($this->tablename, function (Blueprint $table) {
+            $table->bigIncrements('id'); 
+            $table->bigInteger('product_id');
+            $table->string('reference_id'); 
+            $table->bigInteger('quantity');     
+            $table->decimal('unit_selling_price', 5, 2);     
+            $table->decimal('unit_purchase_price', 5, 2);  
+            $table->enum('status',array('ACTIVE','ARCHIVED'));            
+            $table->bigInteger('created_by');
+            $table->timestamp('date_created')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->bigInteger('updated_by')->nullable();
+            $table->datetime('date_updated')->nullable();             
+             
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+         Schema::dropIfExists($this->tablename);
+    }
+}
