768125be69042dca43a956c802c4f98881060d73
Muyinda Rogers <moverr@gmail.com>
2019-02-01 11:23:17 +0300

    :Debugging :

--- tinggengine/database/migrations/2019_01_31_211746_stockists.php
+++ tinggengine/database/migrations/2019_01_31_211746_stockists.php
@@ -23,13 +23,21 @@ class Stockists extends Migration
             $table->datetime('join_date');
             $table->bigInteger('user_id');
             $table->enum('status',array('ACTIVE','ARCHIVED'));            
-            $table->bigInteger('created_by');
+            $table->bigInteger('created_by')->unsigned();
             $table->timestamp('date_created')->default(DB::raw('CURRENT_TIMESTAMP'));
-            $table->bigInteger('updated_by')->nullable();
+            $table->bigInteger('updated_by')->nullable()->unsigned();
             $table->datetime('date_updated')->nullable();  
             
              
         });
+
+         Schema::table($this->tablename, function (Blueprint $table) {  
+            $table->foreign('created_by')->references('id')->on('users');
+            $table->foreign('updated_by')->references('id')->on('users');             
+             
+        });
+
+
 
     }
 
