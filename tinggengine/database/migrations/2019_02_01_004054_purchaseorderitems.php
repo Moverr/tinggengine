e16a46cb319f428c1fa092fbfe88a3afc01155fb
Muyinda Rogers <moverr@gmail.com>
2019-02-01 03:49:52 +0300

    :Debugigng , and adding purchase order items :

--- tinggengine/database/migrations/2019_02_01_004054_purchaseorderitems.php
+++ tinggengine/database/migrations/2019_02_01_004054_purchaseorderitems.php
@@ -4,26 +4,30 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Purchaseorders extends Migration
+class Purchaseorderitems extends Migration
 {
     /**
      * Run the migrations.
      *
      * @return void
      */
-     private  $tablename = 'purchaseorders';
+   private  $tablename = 'purchaseorderitems';
     
 
+/*
+TODO: MEASUREMENT SCENARIOS IS AN IMPORTANT ASPECT OF THIS APPLICATION 
+ */
 
     public function up()
     {
         Schema::create($this->tablename, function (Blueprint $table) {
             $table->bigIncrements('id'); 
-            $table->bigInteger('stockist_id'); 
-            $table->datetime('order_date')->nullable();  
-            $table->string('reference_id')->unique();     
-            $table->decimal('total_amount', 5, 2);  
-            $table->enum('status',array('PENDING','APPROVED','REJECTED','REVERSED','ARCHIVED'));            
+            $table->bigInteger('purchase_order_id');
+            $table->bigInteger('product_id'); 
+            $table->bigInteger('quantity');     
+            $table->decimal('unit_selling_price', 5, 2);     
+            $table->decimal('total_selling_price', 5, 2);  
+            $table->enum('status',array('ACTIVE','ARCHIVED'));            
             $table->bigInteger('created_by');
             $table->timestamp('date_created')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->bigInteger('updated_by')->nullable();
