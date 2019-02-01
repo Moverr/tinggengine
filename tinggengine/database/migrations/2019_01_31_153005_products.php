a06e2e68c071b9c23712acaa07dfa93b049cd4f1
Muyinda Rogers <moverr@gmail.com>
2019-02-01 10:48:18 +0300

    :improving reference constraints :

--- tinggengine/database/migrations/2019_01_31_153005_products.php
+++ tinggengine/database/migrations/2019_01_31_153005_products.php
@@ -16,17 +16,31 @@ class Products extends Migration
 
     public function up()
     {
+        Schema::enableForeignKeyConstraints();
+
         Schema::create($this->tablename, function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name')->unique();
             $table->string('code')->unique();
-            $table->bigInteger('category_id');
+            $table->bigInteger('category_id')->unsigned();
             $table->enum('status',array('ACTIVE','ARCHIVED'));
-            $table->bigInteger('created_by');
+            $table->bigInteger('created_by')->unsigned();
             $table->timestamp('date_created')->default(DB::raw('CURRENT_TIMESTAMP'));
-            $table->bigInteger('updated_by')->nullable();
-            $table->datetime('date_updated')->nullable(); 
+            $table->bigInteger('updated_by')->nullable()->unsigned();
+            $table->datetime('date_updated')->nullable();  
+
         });
+
+
+         Schema::table('products', function (Blueprint $table) {        
+
+            $table->foreign('created_by')->references('id')->on('users');
+            $table->foreign('updated_by')->references('id')->on('users');            
+            $table->foreign('category_id')->references('id')->on('productcategories');
+             
+        });
+
+
 
     }
 
