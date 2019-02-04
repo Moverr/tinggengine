295d56753333c99eb2ad20cb6593a7d6ff70c4f1
Muyinda Rogers <moverr@gmail.com>
2019-02-05 02:58:33 +0300

    :Debugging :

--- tinggengine/app/Http/Controllers/ProductCategoryController.php
+++ tinggengine/app/Http/Controllers/ProductCategoryController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Helpers\Utils;
-
 use App\ProductCategories;
+use App\Http\Controllers\ResponseEntities\ProductResponse;
 
 class ProductCategoryController extends Controller {
 
@@ -18,16 +18,30 @@ class ProductCategoryController extends Controller {
     public function index(Request $request, $offset = 0, $limit = 10) {
         $authentic = $request->header('authentication');
         $autneticaton_response = $this->util->validateAuthenction($authentic);
-        
+
         $productCategories = ProductCategories::offset($offset)->limit($limit)->get();
         return json_encode($productCategories);
-        
-        
-        
     }
 
     public function get(Request $request, $id) {
+        $authentic = $request->header('authentication');
+        $autneticaton_response = $this->util->validateAuthenction($authentic);
+
+        $productCategories = ProductCategories::where('id', $id)->get();
+        if ($productCategories == null) {
+            throw new \Symfony\Component\HttpKernel\Exception\BadRequestHttpException("Record does not exist in the daabase");
+        }
+
+
         
+        $productResponse = new ProductResponse();
+        $productResponse->setId($productCategories[0]->id);
+        $productResponse->setCreatedBy(null);
+        $productResponse->setCategory("N/A");
+        $productResponse->setName($productCategories[0]->name);
+        $productResponse->setCode($productCategories[0]->code);
+        $productResponse->setDateCreated("N/A");
+        return $productResponse->toJson();
     }
 
     public function save(Request $request) {
