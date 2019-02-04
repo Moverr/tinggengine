12e1ab57608163b28f38f109c61b7a87c4e3a14f
Muyinda Rogers <moverr@gmail.com>
2019-02-05 02:53:18 +0300

    :Debuging :

--- tinggengine/app/Http/Controllers/ProductCategoryController.php
+++ tinggengine/app/Http/Controllers/ProductCategoryController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Helpers\Utils;
 
+use App\ProductCategories;
+
 class ProductCategoryController extends Controller {
 
     private $util;
@@ -14,6 +16,13 @@ class ProductCategoryController extends Controller {
     }
 
     public function index(Request $request, $offset = 0, $limit = 10) {
+        $authentic = $request->header('authentication');
+        $autneticaton_response = $this->util->validateAuthenction($authentic);
+        
+        $productCategories = ProductCategories::offset($offset)->limit($limit)->get();
+        return json_encode($productCategories);
+        
+        
         
     }
 
