827ce2e147daf32952c364d05b0222c8bc8c2ea1
Muyinda Rogers <moverr@gmail.com>
2019-02-05 18:57:58 +0300

    :Debugingg :|

--- tinggengine/database/seeds/defaultRole.php
+++ tinggengine/database/seeds/defaultRole.php
@@ -2,15 +2,28 @@
 
 use Illuminate\Database\Seeder;
 
-class defaultRole extends Seeder
-{
+class defaultRole extends Seeder {
+
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
-        //
+    public function run() {
+        DB::table('roles')->insert([
+            'id' => 1,
+            'name' => 'SUPER ADMINISTRATOR',
+            'code' => 'SA',
+            'description' => 'SUPER ADMINISTRATOR ROLE,HAS CAPACITY OVER THE ENTIRE SYSTEM',
+            'status' => 'ACTIVE',
+            'is_system' => TRUE
+        ]);
+
+        DB::table('rolepermission')->insert([
+            'id' => 1,
+            'role_id' => 1,
+            'permission_id' => 1
+        ]);
     }
+
 }
--- tinggengine/database/seeds/defaultUser.php
+++ tinggengine/database/seeds/defaultUser.php
@@ -2,15 +2,24 @@
 
 use Illuminate\Database\Seeder;
 
-class defaultUser extends Seeder
-{
+class defaultUser extends Seeder {
+
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
-        //
+    public function run() {
+        DB::table('permissions')->insert([
+            'id' => 1,
+            'name' => 'admin',
+            'password' => sha1("admin123")
+        ]);
+
+        DB::table('userrole')->insert([
+            'user_id' => 1,
+            'role_id' => 1
+        ]);
     }
+
 }
