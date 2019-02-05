28bb6bb31b0f7e65a7a03ae9021ce41f06463630
Muyinda Rogers <moverr@gmail.com>
2019-02-05 13:04:59 +0300

    :Debugging :

--- tinggengine/app/Http/Controllers/PurchaseController.php
+++ tinggengine/app/Http/Controllers/PurchaseController.php
@@ -3,19 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Helpers\Utils;
+use App\Stock;
+use App\Http\Controllers\RequestEntities\StockRequest;
+use App\Stock;
 
-class PurchaseController extends Controller
-{
+class PurchaseController extends Controller {
+
+    private $util;
+
     function __construct() {
-        
+        $this->util = new Utils();
     }
 
     public function index(Request $request, $offset = 0, $limit = 10) {
-        
+        $authentic = $request->header('authentication');
+        $autneticaton_response = $this->util->validateAuthenction($authentic);
+
+        $stock = Stock::offset($offset)->limit($limit)->get();
+        return json_encode($stock);
     }
 
     public function get(Request $request, $id) {
-        
+        $authentic = $request->header('authentication');
+        $autneticaton_response = $this->util->validateAuthenction($authentic);
+
+        $stock = Products::where('id', $id)->get();
+        if ($stock == null) {
+            throw new \Symfony\Component\HttpKernel\Exception\BadRequestHttpException("Record does not exist in the daabase");
+        }
+
+        $productResponse = $this->populate($stock);
+        return $productResponse->toJson();
     }
 
     public function save(Request $request) {
@@ -29,4 +48,5 @@ class PurchaseController extends Controller
     public function archive(Request $request, $id) {
         
     }
+
 }
