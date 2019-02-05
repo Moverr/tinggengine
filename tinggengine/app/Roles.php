beafc99faffcab7b75328f2c20b3c88ef9540f5c
Muyinda Rogers <moverr@gmail.com>
2019-02-05 15:19:40 +0300

    Roles :

--- tinggengine/app/Roles.php
+++ tinggengine/app/Roles.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Roles extends Model
 {
-    //
+   
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'code', 'description', 'is_system', 'status'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+    protected $table = 'roles';
+
+    const CREATED_AT = 'date_created';
+    const UPDATED_AT = 'date_updated';
 }
