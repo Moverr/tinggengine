9dbd89ce09c5c4a9213a17c655c6af253305a658
Muyinda Rogers <moverr@gmail.com>
2019-02-05 14:01:43 +0300

    Permissions :

--- tinggengine/app/Permissions.php
+++ tinggengine/app/Permissions.php
@@ -4,7 +4,23 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Permissions extends Model
-{
-    //
+class Permissions extends Model {
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'code'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+    protected $table = 'permissions';
+
 }
