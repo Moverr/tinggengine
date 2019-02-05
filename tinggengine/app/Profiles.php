595c68c4bfc0f4d0bedcb4f2cfcc0fff3ded46d1
Muyinda Rogers <moverr@gmail.com>
2019-02-05 14:58:43 +0300

    :Debugging :

--- tinggengine/app/Profiles.php
+++ tinggengine/app/Profiles.php
@@ -4,7 +4,26 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Profiles extends Model
-{
-    //
+class Profiles extends Model {
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'firstname', 'lastname', 'status'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+    protected $table = 'profiles';
+
+    const CREATED_AT = 'date_created';
+    const UPDATED_AT = 'date_updated';
+
 }
