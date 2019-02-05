df411602c7f19524bd9a0b41ed6320c3f4dfcc66
Muyinda Rogers <moverr@gmail.com>
2019-02-05 14:00:18 +0300

    Contacts Model :

--- tinggengine/app/Contacts.php
+++ tinggengine/app/Contacts.php
@@ -4,7 +4,27 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Contacts extends Model
-{
-    //
+class Contacts extends Model {
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'profile_id', 'contact_type', 'details',
+        'status'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+    protected $table = 'contacts';
+
+    const CREATED_AT = 'date_created';
+    const UPDATED_AT = 'date_updated';
+
 }
