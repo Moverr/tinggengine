4493d409f014b030f797d163fac2f35e2856092a
Muyinda Rogers <moverr@gmail.com>
2019-02-06 13:17:26 +0300

    Working upon the product categories endpoint :

--- tinggengine/routes/api.php
+++ tinggengine/routes/api.php
@@ -16,12 +16,20 @@
 //Route::middleware('auth:api')->get('/user', function (Request $request) {
 //    return $request->user();
 //});
-
+//User Controller 
 Route::get('users/', 'UsersController@index');
-
 Route::get('users/{id}', 'UsersController@get');
 Route::post('users/', 'UsersController@save');
 Route::post('login/', 'UsersController@login');
 Route::put('users/', 'UsersController@update');
 Route::delete('users/{id}', 'UsersController@archive');
 Route::get('users/{offset}/{limit?}', 'Users@index');
+
+
+//Product Categories
+Route::get('products/categories/', 'UsersController@index');
+Route::get('products/categories//{id}', 'UsersController@get');
+Route::post('products/categories//', 'UsersController@save');
+Route::put('products/categories//', 'UsersController@update');
+Route::delete('products/categories//{id}', 'UsersController@archive');
+Route::get('products/categories//{offset}/{limit?}', 'Users@index');
