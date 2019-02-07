ab115146a9d350b86dc7176004ec48e0f4fce52c
Muyinda Rogers <moverr@gmail.com>
2019-02-07 12:17:01 +0300

    :debugging :

--- tinggengine/app/Http/Controllers/ResponseEntities/StockistResponse.php
+++ tinggengine/app/Http/Controllers/ResponseEntities/StockistResponse.php
@@ -14,5 +14,93 @@ namespace App\Http\Controllers\ResponseEntities;
  * @author mover  
  */
 class StockistResponse {
-    //put your code here
+    private $id;
+    private $firstname;
+    private $lastname;
+    private $companyname;
+    private $countrycode;
+    private $phonenumber;
+    private $username;    
+    private $status;
+    private $datecreated;
+    
+    
+    function __construct() {
+        
+    }
+    
+    function getId() {
+        return $this->id;
+    }
+
+    function getFirstname() {
+        return $this->firstname;
+    }
+
+    function getLastname() {
+        return $this->lastname;
+    }
+
+    function getCompanyname() {
+        return $this->companyname;
+    }
+
+    function getCountrycode() {
+        return $this->countrycode;
+    }
+
+    function getPhonenumber() {
+        return $this->phonenumber;
+    }
+
+    function getUsername() {
+        return $this->username;
+    }
+
+    function getStatus() {
+        return $this->status;
+    }
+
+    function getDatecreated() {
+        return $this->datecreated;
+    }
+
+    function setId($id) {
+        $this->id = $id;
+    }
+
+    function setFirstname($firstname) {
+        $this->firstname = $firstname;
+    }
+
+    function setLastname($lastname) {
+        $this->lastname = $lastname;
+    }
+
+    function setCompanyname($companyname) {
+        $this->companyname = $companyname;
+    }
+
+    function setCountrycode($countrycode) {
+        $this->countrycode = $countrycode;
+    }
+
+    function setPhonenumber($phonenumber) {
+        $this->phonenumber = $phonenumber;
+    }
+
+    function setUsername($username) {
+        $this->username = $username;
+    }
+
+    function setStatus($status) {
+        $this->status = $status;
+    }
+
+    function setDatecreated($datecreated) {
+        $this->datecreated = $datecreated;
+    }
+
+
+
 }
