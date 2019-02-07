3b2a1ced9487225a1c80730f632998f155c49a74
Muyinda Rogers <moverr@gmail.com>
2019-02-07 10:51:49 +0300

    :Debugging :

--- tinggengine/app/Http/Controllers/StockistController.php
+++ tinggengine/app/Http/Controllers/StockistController.php
@@ -3,15 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Helpers\Utils;
+use App\Stock;
+use App\Http\Controllers\RequestEntities\StockRequest;
+use App\Http\Controllers\ResponseEntities\StockResponse;
+use App\StockTransactions;
 
-class StockistController extends Controller
-{
-     function __construct() {
-        
+class StockistController extends Controller {
+
+    private $util;
+
+    function __construct() {
+        $this->util = new Utils();
     }
 
     public function index(Request $request, $offset = 0, $limit = 10) {
-        
+        $authentic = $request->header('authentication');
+        $autneticaton_response = $this->util->validateAuthenction($authentic);
+        $stock = Stock::offset($offset)->limit($limit)->get();
+        $productResponses = [];
+        foreach ($stock as $record) {
+            $productResponses [] = $this->populate($record)->toJson();
+        }
+
+        return $productResponses;
     }
 
     public function get(Request $request, $id) {
@@ -29,4 +44,5 @@ class StockistController extends Controller
     public function archive(Request $request, $id) {
         
     }
+
 }
