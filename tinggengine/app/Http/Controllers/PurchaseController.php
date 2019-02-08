c10be21fe5e4576d2870182b5f16e5d9541f2852
Muyinda Rogers <moverr@gmail.com>
2019-02-08 13:22:20 +0300

    :Debugging :

--- tinggengine/app/Http/Controllers/PurchaseController.php
+++ tinggengine/app/Http/Controllers/PurchaseController.php
@@ -4,10 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Helpers\Utils;
-use App\Stock;
-use App\PurchaseOrders;
 use App\Http\Controllers\ResponseEntities\PurchaseOrderResponse;
-use App\Http\Controllers\RequestEntities\PurchaseOrderRequest;
 use App\Http\Services\PurchaseService;
 
 class PurchaseController extends Controller {
@@ -44,38 +41,7 @@ class PurchaseController extends Controller {
         $authentic = $request->header('authentication');
         $autneticaton_response = $this->util->validateAuthenction($authentic);
 
-
-        $stockist_id = $request['stockist_id'];
-        $order_date = $request['reference_id'];
-        $reference_id = $this->util->incrementalHash(5);
-
-        $stockRequest = new PurchaseOrderRequest();
-
-        if ($request['id'] == null) {
-            throw new \Symfony\Component\HttpKernel\Exception\BadRequestHttpException("Mandatory field ID is missing");
-        }
-
-        $stockRequest->setId($request['id']);
-        $stockRequest->validate();
-
-        $stockRequest = Stock::where('id', $stockRequest->getId())->first();
-        if ($product == null) {
-            throw new \Symfony\Component\HttpKernel\Exception\BadRequestHttpException("Record does not exist in the daabase");
-        }
-
-
-
-
-        $stock->product_id = $product_id;
-        $stock->reference_id = "ReferenceID";
-        $stock->quantity = $quantity;
-        $stock->unit_selling_price = $unit_selling_price;
-        $stock->unit_purchase_price = null;
-        $stock->status = 'ACTIVE';
-        $stock->update();
-
-        $stockResponse = $this->populate($stock);
-        return $stockResponse->toJson();
+        return $this->purchaseService->update($request);
     }
 
     public function archive(Request $request, $id) {
@@ -83,27 +49,7 @@ class PurchaseController extends Controller {
         $authentic = $request->header('authentication');
         $autneticaton_response = $this->util->validateAuthenction($authentic);
 
-
-        $product = ProductCategories::where('id', $id)->first();
-        if ($product == null) {
-            throw new \Symfony\Component\HttpKernel\Exception\BadRequestHttpException("Record does not exist in the daabase");
-        }
-        $product->status = 'ARCHIVED';
-        $product->update();
-    }
-
-    public function populate($purchaseorder) {
-        $purchaseorderresponse = new PurchaseOrderResponse();
-        $purchaseorderresponse->setStockist_id($purchaseorder->stockist_id);
-        $purchaseorderresponse->setOrder_date($purchaseorder->order_date);
-        $purchaseorderresponse->setReference_id($purchaseorder->reference_id);
-        $purchaseorderresponse->setTotal_amount($purchaseorder->total_amount);
-        $purchaseorderresponse->setStatus($purchaseorder->status);
-        $purchaseorderresponse->setCreated_by($purchaseorder->created_by);
-        $purchaseorderresponse->setDate_created($purchaseorder->date_created);
-        $purchaseorderresponse->setId($purchaseorder->id);
-
-        return $purchaseorderresponse;
+        return $this->purchaseService->update($id);
     }
 
 }
--- tinggengine/app/Http/Services/PurchaseService.php
+++ tinggengine/app/Http/Services/PurchaseService.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Services;
 
+use Illuminate\Http\Request;
+use App\Http\Helpers\Utils;
+use App\Stock;
 use App\PurchaseOrders;
 use App\Http\Controllers\ResponseEntities\PurchaseOrderResponse;
 use App\Http\Controllers\RequestEntities\PurchaseOrderRequest;
@@ -22,9 +25,10 @@ class PurchaseService {
     //put your code here
 
     private static $instance;
+    private $util;
 
     function __construct() {
-        
+        $this->util = new Utils();
     }
 
     public static function getInstance() {
@@ -55,9 +59,9 @@ class PurchaseService {
         $productResponse = $this->populate($purchaseorder[0]);
         return $productResponse->toJson();
     }
-    
-    public function save($request, $authentication = null){
-         $stockist_id = $request['stockist_id'];
+
+    public function save($request, $authentication = null) {
+        $stockist_id = $request['stockist_id'];
         $order_date = $request['reference_id'];
         $reference_id = $this->util->incrementalHash(5);
         $createdBy = $autneticaton_response->getId();
@@ -84,6 +88,53 @@ class PurchaseService {
 
         $stockResponse = $this->populate($purchaseorder);
         return $stockResponse->toJson();
+    }
+
+    public function update($request, $authentication = null) {
+        $stockist_id = $request['stockist_id'];
+        $order_date = $request['reference_id'];
+        $reference_id = $this->util->incrementalHash(5);
+
+        $stockRequest = new PurchaseOrderRequest();
+
+        if ($request['id'] == null) {
+            throw new \Symfony\Component\HttpKernel\Exception\BadRequestHttpException("Mandatory field ID is missing");
+        }
+
+        $stockRequest->setId($request['id']);
+        $stockRequest->validate();
+
+        $stockRequest = Stock::where('id', $stockRequest->getId())->first();
+        if ($product == null) {
+            throw new \Symfony\Component\HttpKernel\Exception\BadRequestHttpException("Record does not exist in the daabase");
+        }
+
+
+
+
+        $stock->product_id = $product_id;
+        $stock->reference_id = "ReferenceID";
+        $stock->quantity = $quantity;
+        $stock->unit_selling_price = $unit_selling_price;
+        $stock->unit_purchase_price = null;
+        $stock->status = 'ACTIVE';
+        $stock->update();
+
+        $stockResponse = $this->populate($stock);
+        return $stockResponse->toJson();
+    }
+
+    public function archive($id, $authentication = null) {
+        $authentic = $request->header('authentication');
+        $autneticaton_response = $this->util->validateAuthenction($authentic);
+
+
+        $product = ProductCategories::where('id', $id)->first();
+        if ($product == null) {
+            throw new \Symfony\Component\HttpKernel\Exception\BadRequestHttpException("Record does not exist in the daabase");
+        }
+        $product->status = 'ARCHIVED';
+        $product->update();
     }
 
     public function populate($purchaseorder) {
