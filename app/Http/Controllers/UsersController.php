314172640cee762d22148106401183d5d153dc82
Muyinda Rogers <moverr@gmail.com>
2019-02-11 19:12:47 +0300

    :Debugging :

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -37,7 +37,7 @@ class UsersController extends Controller {
 
     public function save(Request $request) {
 
-        $authentic = $request->header('authentication');
+        // $authentic = $request->header('authentication');
         $autneticaton_response = $this->util->validateAuthenction($authentic);
         return $this->userservice->save($request, $autneticaton_response);
     }
