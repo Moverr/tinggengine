31c2d388581425dce255e6cb2215ad01cb24a6dd
Muyinda Rogers <moverr@gmail.com>
2019-02-25 09:44:31 +0300

    :Debugging :

--- database/seeds/DefaultSysPermissions.php
+++ database/seeds/DefaultSysPermissions.php
@@ -16,8 +16,76 @@ class DefaultSysPermissions extends Seeder {
             'code' => 'ALL_FUNCTIONS'
         ]);
 
-        
 
+        DB::table('permissions')->insert([
+            'id' => 1,
+            'name' => 'FETCH ONLY OWN RECORDS',
+            'code' => 'FOOR'
+        ]);
+        DB::table('permissions')->insert([
+            'id' => 1,
+            'name' => 'FETCH OWN AND CHILDREN RECORDS',
+            'code' => 'FOACR'
+        ]);
+
+
+        //categories
+        DB::table('permissions')->insert([
+            'id' => 2,
+            'name' => 'CREATE PRODUCT CATEGORIES',
+            'code' => 'CPG'
+        ]);
+        DB::table('permissions')->insert([
+            'id' => 3,
+            'name' => 'EDIT PRODUCT CATEGORIES',
+            'code' => 'EPG'
+        ]);
+        DB::table('permissions')->insert([
+            'id' => 4,
+            'name' => 'ARCHIVE PRODUCT CATEGORIES',
+            'code' => 'APG'
+        ]);
+        DB::table('permissions')->insert([
+            'id' => 5,
+            'name' => 'LIST PRODUCT CATEGORIES',
+            'code' => 'LPG'
+        ]);
+        DB::table('permissions')->insert([
+            'id' => 1,
+            'name' => 'GET PRODUCT CATEGORIES',
+            'code' => 'GPG'
+        ]);
+
+
+ //products
+        DB::table('permissions')->insert([
+            'id' => 2,
+            'name' => 'CREATE PRODUCT ',
+            'code' => 'CP'
+        ]);
+        DB::table('permissions')->insert([
+            'id' => 3,
+            'name' => 'EDIT PRODUCT ',
+            'code' => 'EP'
+        ]);
+        DB::table('permissions')->insert([
+            'id' => 4,
+            'name' => 'ARCHIVE PRODUCT ',
+            'code' => 'AP'
+        ]);
+        DB::table('permissions')->insert([
+            'id' => 5,
+            'name' => 'LIST PRODUCT ',
+            'code' => 'LP'
+        ]);
+        DB::table('permissions')->insert([
+            'id' => 1,
+            'name' => 'GET PRODUCT ',
+            'code' => 'GP'
+        ]);
+
+
+        
 
         DB::table('roles')->insert([
             'id' => 1,
@@ -32,27 +100,22 @@ class DefaultSysPermissions extends Seeder {
             'role_id' => 1,
             'permission_id' => 1
         ]);
-        
-         
-        
+
+
+
         DB::table('users')->insert([
-            'id'=>1,
+            'id' => 1,
             'username' => 'admin',
             'password' => sha1('admin@123'),
             'status' => 'ACTIVE',
             'group' => 'ADMINISTRATOR'
-            
         ]);
-        
-        
+
+
         DB::table('userrole')->insert([
             'user_id' => 1,
-            'role_id' => 1 
+            'role_id' => 1
         ]);
-        
-        
-        
-        
     }
 
 }
